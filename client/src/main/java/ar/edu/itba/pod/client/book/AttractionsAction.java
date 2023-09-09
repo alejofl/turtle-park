b8336e38a08d6bb3e85df794ae27b0ab430c0e09
Alejo Flores Lucey <alejo@misterflores.com>
2023-09-09 18:58:37 -0300

    Implement AttractionsAction from BookClient

--- client/src/main/java/ar/edu/itba/pod/client/book/AttractionsAction.java
+++ client/src/main/java/ar/edu/itba/pod/client/book/AttractionsAction.java
@@ -1,8 +1,18 @@
 package ar.edu.itba.pod.client.book;
 
+import ar.edu.itba.pod.admin.AdminServiceGrpc;
+import ar.edu.itba.pod.admin.CapacityRequest;
+import ar.edu.itba.pod.admin.CapacityResponse;
+import ar.edu.itba.pod.book.BookServiceGrpc;
+import ar.edu.itba.pod.book.RideResponse;
 import ar.edu.itba.pod.client.Action;
+import ar.edu.itba.pod.client.Util;
+import ar.edu.itba.pod.commons.Empty;
 import io.grpc.ManagedChannel;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
 
+import java.awt.print.Book;
 import java.util.List;
 
 public class AttractionsAction extends Action {
@@ -12,7 +22,25 @@ public class AttractionsAction extends Action {
 
     @Override
     public void run(ManagedChannel channel) {
-        //TODO
+        BookServiceGrpc.BookServiceBlockingStub stub = BookServiceGrpc.newBlockingStub(channel);
+
+        try {
+            RideResponse response = stub.getRides(Empty.newBuilder().build());
+            System.out.printf("%-40s | Opening Time | Closing Time\n", "Attraction");
+            response.getRidesList().stream().map(ride -> String.format(
+                                                            "%-40s | %12s | %12s",
+                                                            ride.getRideName(),
+                                                            ride.getOpeningTime(),
+                                                            ride.getClosingTime()
+                                                         )
+                                                ).forEach(System.out::println);
+        } catch (StatusRuntimeException e) {
+            if (e.getStatus() == Status.INVALID_ARGUMENT) {
+                throw new IllegalArgumentException();
+            }
+            System.err.println(Util.GENERIC_ERROR_MESSAGE);
+            System.exit(1);
+        }
     }
 
     @Override
